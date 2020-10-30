4b131589366348be3067f810c968ad9e83436ab5
baimag <baimag02@yandex.ru>
2020-10-30 19:13:11 +0300

    homework

--- src/index.js
+++ src/index.js
@@ -55,7 +55,13 @@ const words2 = words.map(function(e) {
 
 //задание 4
 
-
+//было
+    //function sendMessage(message) {
+    /*
+    * тут какой-то большой код для отправки сообщения
+    */
+//}
+//стало
  const sendMessage = (message = 10) => {
         if(message.length > 999){
            return ("длинное сооб")
@@ -63,34 +69,67 @@ const words2 = words.map(function(e) {
     return ("норм")
 }
 
-
+//было
+//function half(number) {
+//    return number / 2;
+//}
+//стало
  const half = (number) => {
      return number / 2;
  }
 
-
+//было
+//function showConsole() {
+//    console.log('intocode');
+//}
+//
+//стало
  const showConsole = () => {
      console.log("intocode")
  }
 
+//было
+//function concatWords(first, second) {
+//    return first + second;
+//}
 
+//стало
 const contacWords = (first, second) => {
      return  first + second;
 }
-
+//было
+//const justText = () => 'intocode';
+//стало
 function justText(){
      return "intocode";
 }
-
+//было
+//const logging = text => console.log(text);
+//стало
 function logging(text="text"){
      console.log(text)
 }
-
+//было
+//
+//const add = (x,y) => {
+//    const z = 3;
+//
+ //   return z * x * y;
+//}
+//стало
 function add(x = 2,y= 3){
      const z = 3;
      return z * x * y;
 }
-
+//было
+//const onlyPositive = (number) => {
+//    if(number < 0) {
+//        return false;
+//    }
+//
+//    return true;
+//}
+//стало
 function onlyPositive(number= 10 ){
      if (number < 0){
          return false;
